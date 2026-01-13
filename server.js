f023392e142ae8def8be9b8d1b12cc79d15285cd
Jestri03 <jestriilham12.3@gmail.com>
2026-01-13 21:35:17 +0700

    Fix: Route Control and Admin Dashboard

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-// 1. HALAMAN PEMBELI
+// 1. ROUTE BERANDA
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// 2. HALAMAN ADMIN (Tambah/Hapus Buku)
+// 2. ROUTE ADMIN (JESTRI CONTROL)
 app.get('/jestri-control', (req, res) => {
     res.render('admin', { mode: 'login' });
 });
 
 app.post('/admin-dashboard', (req, res) => {
-    if (req.body.password === 'JESTRI0301209') {
+    const pass = req.body.password;
+    if (pass === 'JESTRI0301209') {
         res.render('admin', { mode: 'katalog', books: [] });
     } else {
         res.send("<script>alert('Password Salah!'); window.location='/jestri-control';</script>");
     }
 });
 
-// 3. HALAMAN WATERMARK LAB (Link Rahasia Baru)
+// 3. ROUTE WATERMARK LAB
 app.get('/jestri-lab', (req, res) => {
     res.render('lab');
 });
