f915e00327601a8b8e1110dd33cb05df8c344276
Jestri03 <jestriilham12.3@gmail.com>
2026-01-14 17:38:43 +0700

    Fix: All errors and config

--- server.js
+++ server.js
@@ -6,46 +6,57 @@ const app = express();
 
 const mongoURI = 'mongodb+srv://JESTRI:JESTRI0301209@cluster0.tprp2r7.mongodb.net/ebook_jestri?retryWrites=true&w=majority';
 
-mongoose.connect(mongoURI).catch(err => console.log("DB Error"));
+// Koneksi Database dengan opsi stabil
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB Connect'))
+    .catch(err => console.log('DB Error: ' + err));
 
-const Buku = mongoose.model('Buku', { judul: String, penulis: String, harga: Number, gambar: String });
+const Buku = mongoose.model('Buku', { 
+    judul: String, penulis: String, harga: Number, gambar: String 
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cookieSession({ name: 'session', keys: ['jestri-key'], maxAge: 24 * 60 * 60 * 1000 }));
+app.use(cookieSession({ 
+    name: 'session', 
+    keys: ['jestri-secret'], 
+    maxAge: 24 * 60 * 60 * 1000 
+}));
 
+// HALAMAN UTAMA
 app.get('/', async (req, res) => {
     try {
-        const data = await Buku.find();
+        const data = await Buku.find() || [];
         res.render('index', { buku: data });
-    } catch (e) { res.send("Error Database"); }
+    } catch (e) {
+        res.render('index', { buku: [] });
+    }
 });
 
+// LOGIN
 app.get('/login', (req, res) => res.render('login'));
 app.post('/admin-dashboard', (req, res) => {
     if (req.body.password === 'JESTRI0301209') {
         req.session.admin = true;
         res.redirect('/jestri-control');
-    } else { res.send("Password Salah!"); }
+    } else {
+        res.send("Password Salah!");
+    }
 });
 
+// ADMIN
 app.get('/jestri-control', async (req, res) => {
     if (!req.session.admin) return res.redirect('/login');
-    const data = await Buku.find();
+    const data = await Buku.find() || [];
     res.render('admin', { buku: data });
 });
 
 app.post('/tambah-buku', async (req, res) => {
     if (req.session.admin) {
-        await new Buku({ 
-            judul: req.body.judul, 
-            penulis: req.body.penulis, 
-            harga: Number(req.body.harga), 
-            gambar: req.body.gambar 
-        }).save();
+        const { judul, penulis, harga, gambar } = req.body;
+        await new Buku({ judul, penulis, harga: Number(harga), gambar }).save();
     }
     res.redirect('/jestri-control');
 });
